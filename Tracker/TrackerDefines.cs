a416953943d0ee5dfe71d8fad4d68beeafbe948b
Jens Fiedler <Jens.Fiedler@FormFactor.com>
2021-01-29 10:43:43 +0100

    separate tracking into separate library

--- Tracker/TrackerDefines.cs
+++ Tracker/TrackerDefines.cs
@@ -4,8 +4,8 @@
     {
         public const int IDLE_TIMEOUT_IN_MINUTES = 5; //Time in minutes after which user is tracked as inactive if no mouse or keyboard action happened in the meantime
         public const int FILE_BACKUP_TIME_IN_MINUTES = 5; //perform a backup of the data to a csv file after this many minutes
-        public const string ActivityrawFile = "ActivityRaw1_";
-        public const string ActivityCombinedFile = "ActivityCombined1_";
+        public const string ActivityrawFile = "ActivityRaw_";
+        public const string ActivityCombinedFile = "ActivityCombined_";
         public const string ActivityFileExtension = ".csv";
     }
 }
